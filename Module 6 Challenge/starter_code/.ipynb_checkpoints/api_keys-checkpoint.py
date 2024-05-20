ee51c7f7a829831f99d83023ca6fe640926e96d5
Ashley Anderson <151413928+AshleyKAnderson@users.noreply.github.com>
2024-05-20 13:23:05 -0500

    Delete Module 6 Challenge directory

--- Module 6 Challenge/starter_code/.ipynb_checkpoints/api_keys-checkpoint.py
+++
@@ -1,5 +0,0 @@
-# OpenWeatherMap API Key
-weather_api_key = "f61a9228124728fe6e4699cc8d8e7be3"
-
-# Geoapify API Key
-geoapify_key = "70c6396441684e8690aa1fb45dff670e"
--- Module 6 Challenge/starter_code/api_keys.py
+++
@@ -1,5 +0,0 @@
-# OpenWeatherMap API Key
-weather_api_key = "f61a9228124728fe6e4699cc8d8e7be3"
-
-# Geoapify API Key
-geoapify_key = "c9f45d607fa549d0ac1ad988564e1feb"
